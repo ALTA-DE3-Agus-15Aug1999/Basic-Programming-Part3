5ed30ac7ce2e5432ad9b17c23f9736d6157f24f4
agusfn15 <nagusfernando@gmail.com>
2024-05-14 14:10:08 +0700

    solve problem 1

--- problem1/main.py
+++ problem1/main.py
@@ -2,6 +2,21 @@
 student_score = 80
 
 # Process: Your Solution Code Here
+num = int(student_score)
+grade = ""
 
+if num >= 80:
+        grade = "A"
+elif num >= 50:
+    if num >= 65:
+        grade = "B+"
+    else:
+        grade = "B"
+elif num >= 35:
+    grade = "C"
+else:
+    grade = "D"
+
+print("Nilai", grade)
 
 # Output "Nilai A"
