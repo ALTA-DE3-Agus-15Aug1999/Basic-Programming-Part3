1e46920ade5009fdebcdd4165ee04f7bb55f1611
agusfn15 <nagusfernando@gmail.com>
2024-05-14 14:15:26 +0700

    solve problem 2.1

--- problem2.1/main.py
+++ problem2.1/main.py
@@ -1,1 +1,11 @@
-bilangan = int(input())
+def faktor_bilangan(bilangan):
+    m = []
+    for i in range(1, bilangan + 1):
+        if bilangan % i == 0:
+            m.append(i)
+    return m
+
+bilangan = int(input("Masukkan bilangan = "))  
+m = faktor_bilangan(bilangan)  
+for factor in m:    # mengurutkan
+    print(factor)  
