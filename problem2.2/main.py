3f202db8dd6443309a8346560b73ca66c9440482
agusfn15 <nagusfernando@gmail.com>
2024-05-14 14:18:54 +0700

    solve problem 2.2

--- problem2.2/main.py
+++ problem2.2/main.py
@@ -1,1 +1,12 @@
-bilangan = int(input())
+def faktor_bilangan(bilangan):
+    m = []
+    for i in range(1, bilangan + 1):
+        if bilangan % i == 0:
+            m.append(i)
+    return m
+
+bilangan = int(input("Masukkan nilai = "))  
+m = faktor_bilangan(bilangan)  
+m.sort(reverse=True)  # mengurutkan m secara descending
+for factor in m:
+    print(factor)   
