c08bc327bd27534b600b5ff6ea477a5a68442bd7
agusfn15 <nagusfernando@gmail.com>
2024-05-14 14:57:44 +0700

    solve problem 3

--- problem3/main.py
+++ problem3/main.py
@@ -1,5 +1,20 @@
 def prime_number(num):
-    return "error response'
+    if num < 2 :
+        return "Not Prime"
+    for i in range(2, num):
+        if num % i == 0:
+            return "Not Prime" 
+    return "Prime"
+    
+
+def main():
+    num = int
+    if num <= 0:
+        return "error response"
+    elif prime_number(num):
+        return "Prime"
+    else:
+        return "Not Prime"
 
 if __name__ == '__main__':
     print(prime_number(11)) # "Prime"
